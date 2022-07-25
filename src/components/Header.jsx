88c64d77bbcd8f60bcbeb790c0dc358bc192ba42
Matt Millican <matt@mattmillican.com>
2022-07-24 23:15:08 -0500

    Add mobile nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'preact/hooks';
+
+export default function Header() {
+  const [ showMobileNav, setShowMobileNav ] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setShowMobileNav(!showMobileNav);
+  }
+
+  return (
+    <header class="py-4 border-b-4 border-secondary">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <nav class="relative flex justify-between">
+          <a href="/" class="text-primary font-semibold text-3xl hover:no-underline">
+            Matt Millican
+          </a>
+          <div class="-mr-2 -my-2 md:hidden">
+            <button type="button" onClick={toggleMobileMenu} class="p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary" aria-expanded="false">
+              <span class="sr-only">Open menu</span>
+              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
+          </div>
+          <div class="hidden md:flex md:gap-x-6 nav-items">
+            <a href="/about" class="nav-link">About</a>
+            <a href="/posts" class="nav-link">Blog</a>
+            <a href="/projects" class="nav-link">Projects</a>
+            <a href="/uses" class="nav-link">Uses</a>
+          </div>
+        </nav>
+      </div>
+
+      { showMobileNav &&
+        (
+        <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top right md:hidden">
+          <div class="rounded-lg shadow-lg bg-gray-dark border-2 border-gray-400">
+            <div class="pt-5 pb-6 px-5">
+              <div class="flex items-center justify-between">
+                <div>
+                  <a href="/" class="text-primary font-semibold text-3xl hover:no-underline">
+                    Matt Millican
+                  </a>
+                </div>
+                <div class="-mr-2">
+                  <button type="button" onClick={toggleMobileMenu} class="p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary">
+                    <span class="sr-only">Close menu</span>
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div class="mt-6 px-4 grid gap-y-10">
+                <a href="/about" class="-m-3 text-white font-medium visited:text-white">
+                  About
+                </a>
+                <a href="/posts" class="-m-3 text-white font-medium visited:text-white">
+                  Blog
+                </a>
+                <a href="/projects" class="-m-3 text-white font-medium visited:text-white">
+                  Projects
+                </a>
+                <a href="/uses" class="-m-3 text-white font-medium visited:text-white">
+                  Uses
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        )
+      }
+    </header>
+  )
+}
+
