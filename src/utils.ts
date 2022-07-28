54fc5c025c050a37cebbc27690bf79ab77ee43cf
Matt Millican <matt@mattmillican.com>
2022-07-27 23:38:08 -0500

    Add formatting to post dates

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,14 @@
+import { DateTime } from 'luxon';
+
 export function getPostSlugFromFilename(filename: string): string {
   const slugRegex = /(?<=\d{4}-\d{2}-\d{2}-)(?:[\w-]*)/;
   const slug = slugRegex.exec(filename)[0];
   return slug;
 };
+
+export function formatDate(input: string): string {
+  const date = DateTime.fromISO(input);
+
+  const formatted = date.toFormat('LLLL dd, yyyy');
+  return formatted;
+}
