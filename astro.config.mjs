e8880a4bf74d3621f87abf8b7e4d24f9edc9ca28
Matt Millican <matt@mattmillican.com>
2023-03-09 21:31:19 -0600

    Update Astro to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,23 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 import preact from '@astrojs/preact';
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'; // https://astro.build/config
+
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		preact(),
-		tailwind({
-			config: {
-				applyAstroStyles: false,
-			},
-		}),
-    partytown({
-      config: { forward: ['dataLayer.push'] },
-    }),
-	],
+  integrations: [preact(), tailwind({
+    config: {
+      applyAstroStyles: false
+    }
+  }), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })],
   markdown: {
-    drafts: true,
+    drafts: true
   },
-	site: 'https://mattmillican.com',
-	trailingSlash: 'ignore',
+  site: 'https://mattmillican.com',
+  trailingSlash: 'ignore'
 });
